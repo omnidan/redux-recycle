62c5ae8b928cd80696822ac324ac92c59507b918
vzaidman <vzaidman@gmail.com>
2017-07-09 13:38:14 +0300

    added configurable "recycleActionType"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 // redux-recycle higher order reducer
-export default function recycleState (reducer, actions = [], initialState) {
+export default function recycleState (reducer, actions, initialState, config = {}) {
   const getInitialState = (typeof initialState === 'function') ? initialState : () => initialState
+  const recycleActionType = (typeof config.recycleActionType === 'undefined') ? '@@redux-recycle/INIT' : config.recycleActionType
 
   return (state, action) => {
-    if (actions.indexOf(action.type) >= 0) {
-      return reducer(getInitialState(state, action), { type: '@@redux-recycle/INIT' })
+    if (actions.indexOf(action.type) === -1) {
+      return reducer(state, action)
     }
-    return reducer(state, action)
+
+    if (recycleActionType) {
+      return reducer(getInitialState(state, action), { type: recycleActionType })
+    }
+
+    return getInitialState(state, action)
   }
 }
 // /redux-recycle
